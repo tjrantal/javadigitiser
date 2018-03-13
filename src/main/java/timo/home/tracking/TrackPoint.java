2bce9623fa0d3e6c8ccc503fd404753fb15c9215
Timo Rantalainen <tjrantal@gmail.com>
2018-03-13 07:59:54 +0200

    Flood fill seems to work

--- src/main/java/timo/home/tracking/TrackPoint.java
+++ src/main/java/timo/home/tracking/TrackPoint.java
@@ -1,5 +1,6 @@
 package timo.home.tracking;
 
+import java.util.Vector;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Collections;
@@ -20,6 +21,7 @@ public class TrackPoint{
 	private ArrayList<PointHelper> neighbourhood;
 	private int width;
 	private int height;
+	private byte[][] fillMask;
 	
 	public TrackPoint(BIWithMeta currentFrame,double[] digitisedCoordinates,int colourTolerance){
 		this.currentFrame = currentFrame;
@@ -28,12 +30,17 @@ public class TrackPoint{
 		width = currentFrame.getWidth();
 		height = currentFrame.getHeight();
 		neighbourhood = getNeighbourhood(searchRadius);
-		colouredFrame = deepCopy(currentFrame);
-		//Look for the point within the neighbourhood, and colour the colouredFrame
+		
+		//Look for the point within the neighbourhood at this point
+		
+		//TESTING with input point
+		int[] colourIn = getColour((int) digitisedCoordinates[0],(int)  digitisedCoordinates[1]);
+		floodFill((int) digitisedCoordinates[0],(int)  digitisedCoordinates[1],colourIn[0], colourIn[1], colourIn[2], colourTolerance);
+		
 	}
 	
-	
-	private BufferedImage deepCopy(BufferedImage bi) {
+	/*Clone the original frame for colouring*/
+	private BufferedImage clone(BufferedImage bi) {
 	 ColorModel cm = bi.getColorModel();
 	 boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
 	 WritableRaster raster = bi.copyData(null);
@@ -44,17 +51,62 @@ public class TrackPoint{
 		return colouredFrame;
 	}
 	
-	/**Flood fill implemented as recursive functions (i.e floodFill calls floodFill)
-		Took the idea from https://github.com/pedrovgs/Algorithms/blob/master/src/main/java/com/github/pedrovgs/problem42/FloodFill.java
+
+	/**Flood fill marker
 	*/
 	
-	private void floodFill(int x, int y, int r, int g, int b, int tol){
+	private boolean floodFill(int x, int y, int r, int g, int b, int tol){
+		colouredFrame = clone(currentFrame);
+		fillMask = new byte[width][height];
 		
+		int[][] nHood = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};	//4-connected neighbourhood
+		Vector<int[]> seedList = new Vector<int[]>();
+		seedList.add(new int[]{x,y});
+		while (seedList.size() > 0){
+			int[] check = seedList.remove(0);	//Get the first seed, and remove from the list
+			int[] colour = getColour(check[0], check[1]);
+			//Check whether colours are within tolerance
+			if (colour[0] >= r-tol & colour[0] <= r+tol &
+				colour[1] >= g-tol & colour[1] <= g+tol &
+				colour[2] >= b-tol & colour[2] <= b+tol &
+				fillMask[check[0]][check[1]] == 0	//Hasn't been filled yet
+				){
+				fillMask[check[0]][check[1]] = 1;
+				colour[0]+= 50;
+				colour[1]+= 50;
+				colour[2]+= 50;
+				for (int c = 0;c<colour.length;++c){
+					colour[c] = colour[c]<256 ? colour[c]:255;
+				}
+				colourPixel(check[0],check[1],colour);
+				//Add neighbouring pixels if appropriate
+				for (int nh = 0; nh<nHood.length;++nh){
+					int tempX = check[0]+nHood[nh][0];
+					int tempY = check[1]+nHood[nh][1];
+					if ((tempX >-1 & tempX < width & tempY > -1 & tempY < height) && fillMask[tempX][tempY] == 0){
+						seedList.add(new int[]{tempX,tempY});
+					}
+				}
+				
+			}
+		
+		}
+		return true;
 	}
 	
-	private int getValue(){
-		return 0;
+	private int[] getColour(int x, int y){
+		int pixelcolour = currentFrame.getRGB(x,y);
+		int tempR = 0xff & (pixelcolour >> 16);
+		int tempG = 0xff & (pixelcolour >> 8);
+		int tempB = 0xff & pixelcolour;
+		return new int[]{tempR,tempG,tempB};
 	}
+	
+	private void colourPixel(int x, int y, int[] colour){
+		int pixelcolour = 0xff<<24 | colour[0] << 16 | colour[1]<<8 | colour[2];
+		colouredFrame.setRGB(x,y,pixelcolour);
+	}
+
 
 	/**Get pixel neighbourhood coordinates in a concentric circle
 		Will be sorted so that the search proceeds outwards
